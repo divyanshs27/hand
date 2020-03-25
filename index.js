b2e458d0cd0e9d7fb93d71a3dd7c4a107f49f2de
Divyansh Singhal <54583331+divyanshs27@users.noreply.github.com>
2020-03-25 22:29:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var MongoClient = require("mongodb").MongoClient;
-var ObjectID = require("mongodb").ObjectID;
-const dbname = "handwriting_recog";
-const url ="mongodb://localhost:27017/handwriting_recog";
-const mongoOptions = { useNewUrlParser : true};
+const express = require('express');
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const dbname = "page";
+const url = "mongodb+srv://divyansh:divyansh27@cluster0-gd3lq.mongodb.net/h4t";
+const mongoOptions = {useNewUrlParser : true};
 
 const app = express();
 
+let ejs = require('ejs')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname));   // used to add external css files 
-
+app.use(express.static(__dirname));    // this cmd allows external css files to be used
+app.set('view engine', 'ejs');
 app.listen(3000,()=>{
-    MongoClient.connect(url,mongoOptions,(err,client)=>{
-        if(err) 
-        throw err;
-        
-        let database = client.db(dbname);
-        let collection = database.collection("logindata");
-        console.log("Connected to`" + dbname + "`!");
+    MongoClient.connect(url,mongoOptions,(error,client)=>{
+    if(error) 
+    throw(error);
+
+    let database = client.db(dbname);
+    let collection = database.collection("contests");
+    console.log("Connected to`" + dbname + "`!");
     });
 });
 
@@ -27,22 +28,22 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/REGISTER.html");
 });
 app.get('/',(req,res)=>{
-    res.render("index",{});
+    res.render("login",{});
 });
- app.post('/about.html',(req,res)=>{
+ app.post('/about',(req,res)=>{
      
  MongoClient.connect(url,(err,db)=>{
         if(err)
         throw(err);
         let newData =
         {
-            email:req.body.email1,
+            name: req.body.email1,
             username: req.body.username1,
-            password: req.body.password1
+            password: req.body.password1,
+            //confirmPassword: req.body.conpass
         }
-        let dbo = db.db("handwriting_recog");
-        dbo.collection("logindata").insertOne(newData);
+        let dbo = db.db("page");
+        dbo.collection("page").insertOne(newData);
     })
-    res.redirect("about.html") ;     // or use res.end()
+    res.redirect("about.html")      // or use res.end()
 })
-
